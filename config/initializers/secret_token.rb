3a8a0680f469c79d37372f1b0a989ac3f735d128
nikoma <niko.manek@gmail.com>
2012-10-27 15:18:06 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nadiatutor::Application.config.secret_token = '45215e85bcda4b4b31f6f0342e24ab1e56b0655a53d6623223c5d05b9e86ef86d859b434ad23529f2dc10a45ad4b742fab3871d601a04b3da23971ab934946e7'
